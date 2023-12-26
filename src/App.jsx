a07b4524a754eee13f7ce9996e81a121b62269e2
DylanW22 <dylan.whitehouse22@gmail.com>
2023-12-26 09:23:47 +0800

    commitment

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
 
   return (
     <div className="flex flex-col bg-blue-300 justify-center items-center p-4">
-      <h1 className="text-white font-bold text-xl">This is my template.</h1>
+      <h1 className="text-white font-bold text-xl">This is my edited template.</h1>
       <p className="text-white font-bold text-xl">It includes Tailwind, and React-Router pre-configured.</p>
       <p
       className="text-white font-bold text-xl"
